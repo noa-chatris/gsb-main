5a8163306b8530427b8c629d40639fa6ff022b4d
noa-chatris <noa.chatris.2011@gmail.com>
2025-05-20 00:07:14 +0200

    truc css html pour comptable et admin

--- gsb-main/comptable/index.php
+++ gsb-main/comptable/index.php
@@ -18,6 +18,7 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
+        <link rel="icon" type="image/x-icon" href="../src/gsb logo 2">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Intranet - Comptable</title>
         <link rel="stylesheet" href="./style.css">
