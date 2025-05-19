94a556d2a30ad8e6fa85bf3a4e1d90f2a67c93f5
noa-chatris <noa.chatris.2011@gmail.com>
2025-05-19 22:38:58 +0200

    .

--- gsb-main/connect.php
+++ gsb-main/connect.php
@@ -1,8 +1,8 @@
 <?php
     $dsn = 'mysql:host=localhost;dbname=gsb;charset=utf8';
     $user = 'root';
-    $password = '';
-    //$password = "root";
+    //$password = '';
+    $password = "root";
     try {
         $connexion = new PDO($dsn, $user, $password);
     } catch (PDOException $e) {
